c049dc9414d9ce5118e628a80b707d81b63d1964
nacho lopez <nachete-1995@hotmail.com>
2018-11-14 11:55:08 +0100

    updated tables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-= require_tree .
-= require jquery
-= require jquery_ujs
-= require bootstrap-sprockets
-= require_tree .
+// = require_tree .
+// = require jquery
+// = require jquery_ujs
+// = require bootstrap-sprockets
+// = require_tree .
+// Graphical variables
